9f7951f9121927d6c61cd6fe5aeb026ed0a0faeb
Alex Marz <alexhmarz@gmail.com>
2020-12-15 09:34:28 -0500

    commit tree 7a5f95dc69553558bb677df5be7a69ca30dd1570

---
+++ lib/post.rb
@@ -0,0 +1,18 @@
+class Post
+
+    attr_accessor :title, :author
+
+    # @@all = []
+
+    # def initialize(title, author)
+    #     @title = title
+    #     @author = author
+    # end
+    
+    # def self.all 
+    #     @@all
+    # end
+
+end
+
+
